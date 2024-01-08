53cdef31bbf3cd794cb229a3de5a78fa21afea86
GTTHANU <153344455+GTTHANU@users.noreply.github.com>
2024-01-08 22:56:03 +0530

    Add files via upload

---
+++ Pavan.java
@@ -0,0 +1,19 @@
+public class StudentInformation
+{
+public static void main(String args[])
+{
+printStudentInfo();
+}
+private static void printStudentInfo()
+{
+String CollegeName="AITS";
+String studentName="Thanu";
+int rollNumber=201;
+System.out.println("college name:"+CollegeName);
+System.out.println("student name:"+studentName);
+System.out.println("Roll number:"+rollNumber);
+System.out.println("\n welcome, "+studentName+"!");
+System.out.println("we hope you have a fantastic journey at"+CollegeName+".");
+System.out.println("KILL THE SLEEPING PEOPLE);
+}
+}
